570dd060511a23f4fba58f0fb90a25be0500b580
shaikhakib12 <akibshaikh9709@gmail.com>
2026-07-01 03:10:50 +0530

    add 05_string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,39 @@
+const name = "sk akib"
+const repocount = 6
+
+// console.log(name + repocount + "value") // sk akib6value
+
+// console.log(`My name is ${name} and my repo count is ${repocount}`) // My name is sk akib and my repo count is 6
+
+const gameName = new String("pubgmobile")
+
+// console.log(gameName[0]);
+// console.log(gameName.length);
+
+// console.log(gameName.toUpperCase());
+// console.log(gameName.toLowerCase());
+// console.log(gameName.charAt(3));
+
+
+// console.log(gameName.indexOf("m")); // 6
+
+const newString = gameName.substring(0, 3) // pub
+// console.log(newString);
+
+
+const anotherStrig = gameName.slice(0, 3) // pub
+
+const newstringOne = "     akib     "
+console.log(newstringOne);
+console.log(newstringOne.trim()); // akib
+
+
+const url = "https://githunb.com/akibsk"
+
+console.log(usl.replace("githunb", "github")); //
+
+console.log(url.includes("akib")); // true
+
+
+// console.log
+
