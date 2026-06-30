262eca31b16d184b6a99670c720f64efb3dbd3d4
shaikhakib12 <akibshaikh9709@gmail.com>
2026-07-01 03:20:06 +0530

    06 commited

---
+++ 01_basics/06_nums _and_maths.js
@@ -0,0 +1,17 @@
+const score = 400
+console.log(score) // 400
+
+
+const balance = new Number(100)
+console.log(balance) // [Number: 100]
+
+
+console.log(balance.toString(). length);
+console.log(balance.toFixed(2));
+
+const otherNumber = 123.456789
+
+console.log(otherNumber.toPrecision(4)); // 123.5
+
+const hundreds  = 1000000
+console.log(hundreds.toLocaleString('en-IN')); // 1,000,000
