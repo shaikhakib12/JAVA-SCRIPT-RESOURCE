bc8df6399221d5246e9f97612a7f5fa71bf37e92
shaikhakib12 <akibshaikh9709@gmail.com>
2026-07-01 01:28:36 +0530

    learned basics

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 123456789
+let accountEmail = "umar@goggle.com"
+var accountPassword = "123456789"
+accountCity = "kolkata"
+
+//accountId = 144545
+
+
+accountEmail = "hc@google.com"
+accountPassword = "56789"
+accountCity = "delhi"
+
+console.log(accountId);
+
+console.table({accountId, accountEmail, accountPassword, accountCity});
