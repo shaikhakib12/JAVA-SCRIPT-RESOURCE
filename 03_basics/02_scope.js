d7127106dc9ac1e489be685c9a40b5665a1ffe32
shaikhakib12 <akibshaikh9709@gmail.com>
2026-07-03 20:01:13 +0530

    basic scope declarations

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -1,4 +1,56 @@
-let a = 10 
-const b = 20 
-var c = 30
+let c = 300 // global scope 
 
+
+if (true) {
+    let a = 10
+    const b = 20        // block scope cannot access from global scope untill u return 
+    // var c = 30
+    // console.log("INNER: ", a);
+    
+}
+
+// console.log(c);
+
+
+function one(){
+    const username = "akib"
+
+    function two(){
+        const website = "google"
+        console.log(username);
+    }
+    // console.log(website);
+    
+    two()
+}
+
+// one()
+
+if (true) {
+    const username = "akib"
+    if(username === "akib"){
+        const website = " youtube"
+        // console.log(username + website);
+        
+    }
+    // console.log(website);
+    
+}
+// console.log(username);
+
+// ++++++++++++++++++++++++++++ interesting (hoisting) ++++++++++++++++++++++++++++++++++++++++
+
+
+console.log(addone(5))
+
+function addone(num){
+    return num + 1;
+}
+
+
+
+const addTwo = function(num){  // expressions 
+    return num + 2;
+}
+
+addTwo(5)
