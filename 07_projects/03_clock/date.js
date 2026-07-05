660765c8f455d3a82895a85673e220033cf25057
shaikhakib12 <akibshaikh9709@gmail.com>
2026-07-06 01:02:30 +0530

    project 3 complete

---
+++ 07_projects/03_clock/date.js
@@ -0,0 +1,10 @@
+const clock = document.getElementById("clock")
+
+
+
+setInterval(function () {
+    let date = new Date()
+    // console.log(date.toLocaleDateString());
+
+    clock.innerText = date.toLocaleTimeString();
+}, 1000)
